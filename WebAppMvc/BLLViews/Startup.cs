c1cea28a417324e9d1df26bf45873f0eeb500651
tymoschuk <tymoschuk@A33COMP12>
2019-02-27 17:10:41 +0200

    created new structure with identity

---
+++ WebAppMvc/BLLViews/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BLLViews.Startup))]
+namespace BLLViews
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
