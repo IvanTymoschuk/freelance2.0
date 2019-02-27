40c64c13bab466ef18668ee35173a2d01f95b140
istep.andriy <istep.andriy@gmail.com>
2019-02-27 23:07:12 +0200

    script bug fixed and DbModels added

--- WebAppMvc/BLLViews/Models/IdentityModels.cs
+++ WebAppMvc/BLLViews/Models/IdentityModels.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -9,13 +11,66 @@ namespace BLLViews.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+
+        public string FullName { get; set; }
+        public City City { get; set; }
+        public string AvaPath { get; set; }
+        public double Raiting { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
             return userIdentity;
         }
+    }
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class Category
+    {
+        public Category()
+        {
+            Jobs = new List<Job>();
+        }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public ICollection<Job> Jobs { get; set; }
+    }
+    public class Jobs
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public City City { get; set; }
+
+        public Category Category { get; set; }
+        public ApplicationUser UserOwner { get; set; }
+    }
+    public class Ticket
+    {
+        public Ticket()
+        {
+            ticketMSGs = new List<TicketMSG>();
+        }
+        public int ID { get; set; }
+        public string Status { get; set; }
+        public virtual ApplicationUser Owner { get; set; }
+        public virtual ICollection<TicketMSG> ticketMSGs { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public string Theme { get; set; }
+    }
+
+    public class TicketMSG
+    {
+        public int ID { get; set; }
+        public string Text { get; set; }
+        public virtual Ticket ticket { get; set; }
+        public ApplicationUser User { get; set; }
+        public DateTime Date { get; set; }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
@@ -24,7 +79,11 @@ namespace BLLViews.Models
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
-        public virtual DbSet<Job> Jobs { get; set; }
+        public virtual DbSet<Jobs> Jobs { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<City> Cities { get; set; }
+        public virtual DbSet<Ticket> Tickets { get; set; }
+        public virtual DbSet<TicketMSG> TicketMSGs { get; set; }
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
